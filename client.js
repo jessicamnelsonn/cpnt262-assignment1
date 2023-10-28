9afbb24b51ae9bebd3af5da3c97b7d2cc5dd20c5
Jessica Nelson <jessica.nelson60@gmail.com>
2023-10-28 08:27:46 -0600

    added collapsible hide/show info toggle

--- client.js
+++ client.js
@@ -1,3 +1,5 @@
+"use strict"
+
 // Array of objects
 const gallery = [
   { imageFile: "optimized-image1.jpg" },
@@ -16,7 +18,6 @@ const container = document.getElementById("gallery-container");
 // Loop through the data array and render images
 gallery.forEach((item) => {
   const images = document.createElement("div");
-  images.className = "gallery";
   images.innerHTML = `
       
   <img src="assets/images/${item.imageFile}">
@@ -24,3 +25,21 @@ gallery.forEach((item) => {
   container.appendChild(images);
 });
 
+let coll = document.getElementsByClassName("collapsible");
+let i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+
+   // Initially hide the content from collapsible
+   const content = coll[i].nextElementSibling;
+   content.style.display = "none";
+}
